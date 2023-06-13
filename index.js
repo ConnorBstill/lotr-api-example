78e6c7d7c7ede4267c5a70d152887a7edf707bb1
ConnorBStill <connor@lamarsoftware.io>
2023-06-12 22:37:23 -0300

    Added stuff

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 // When you first clone this, if you want to run it you have to first run `npm i` and also create a `secret.js` file with your `apiKey`
 
 const fetch = require('node-fetch');
-
-const apiKey = require('./secrets');
+const apiKey = require('./secrets/secrets');
 const apiUrl = 'https://the-one-api.dev/v2';
 
 const main = async () => {
+  // Using .then and .catch
+  fetch(`${apiUrl}/movie`, {
+    method: 'get',
+    headers: {
+      Authorization: `Bearer ${apiKey}`
+    }
+  }).then(response => {
+    console.log(response)
 
-  // fetch(`${apiUrl}/movie`, {
-  //   method: 'get',
-  //   headers: {
-  //     Authorization: `Bearer ${apiKey}`
-  //   }
-  // }).then(response => {
-  //   console.log(response)
-
-  //   response.json().then(data => {
-  //     console.log(data);
-  //   }).catch(err => {
-  //     console.log(err)
-  //   })
-  // }).catch(err => {
-  //   console.log(err)
-  // });
+    response.json().then(data => {
+      console.log(data);
+    }).catch(err => {
+      console.log(err)
+    })
+  }).catch(err => {
+    console.log(err)
+  });
 
   // Using async/await
   try {
@@ -38,9 +37,19 @@ const main = async () => {
     const data = await response.json();
     
     console.log('data', data);
+    data[0].name;
   } catch (err) {
     console.log('err', err);
   }
+}
+
+try {
+
+
+
+
+} catch (err) {
+  console.log()
 }
 
 main();
